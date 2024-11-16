1c74e95b5d503e37faebbf083062f7d34b73da30
Minh Phan <minhsg.phan@gmail.com>
2024-11-16 14:23:18 -0600

    Implement object health

--- Assets/CharacterControl.cs
+++ Assets/CharacterControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     private CharacterController controller;
+    private Health health;
     private Vector3 playerVelocity;
     private float verticalVelocity;
     private float groundedTimer;  
@@ -16,6 +17,7 @@ public class CharacterControl : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        health = GetComponent<Health>();
     }
 
     // Update is called once per frame
@@ -31,23 +33,19 @@ public class CharacterControl : MonoBehaviour
         {
             groundedTimer -= Time.deltaTime;
         }
-
         // slam into the ground
         if (groundedPlayer && verticalVelocity < 0)
         {
             // hit ground
             verticalVelocity = 0f;
         }
-
         // apply gravity always, to let us track down ramps properly
         verticalVelocity -= gravityValue * Time.deltaTime;
 
         // gather lateral input control
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
         // scale by speed
         move *= playerSpeed;
-
         // only align to motion if we are providing enough input
         if (move.magnitude > 0.05f)
         {
@@ -67,11 +65,15 @@ public class CharacterControl : MonoBehaviour
                 verticalVelocity += Mathf.Sqrt(jumpHeight * 2 * gravityValue);
             }
         }
-
         // inject Y velocity before we use it
         move.y = verticalVelocity;
 
         // call .Move() once only
         controller.Move(move * Time.deltaTime);
+
+        if (Input.GetKeyDown("k"))
+        {
+            health.Kill();
+        }
     }
 }
